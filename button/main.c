b189dc6b9b081f7d9848e79728a3ccd64d1e65fc
fireflower0 <fireflower0@hotmail.co.jp>
2020-02-15 00:19:30 +0900

    Add a button in examples

---
+++ button/main.c
@@ -0,0 +1,22 @@
+// gcc -o button main.c -l wiringPi
+#include <wiringPi.h>
+
+#define GPIO14 14
+#define GPIO25 25
+
+int main(void) {
+  if(wiringPiSetupGpio() == -1) {
+    return -1;
+  }
+  pinMode(GPIO14, OUTPUT);
+  pinMode(GPIO25, INPUT);
+  pullUpDnControl(GPIO25, PUD_UP);
+
+  for(;;) {
+    if(digitalRead(GPIO25) == 0){
+      digitalWrite(GPIO14, 1);
+    } else {
+      digitalWrite(GPIO14, 0);
+    }
+  }
+}
