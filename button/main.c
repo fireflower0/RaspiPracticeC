ad19e18651ca589e7bea06863498b9b16b638fd3
fireflower0 <fireflower0@hotmail.co.jp>
2020-02-18 00:01:30 +0900

    Fixed a button

--- button/main.c
+++ button/main.c
@@ -1,22 +1,19 @@
 // gcc -o button main.c -l wiringPi
+#include <stdio.h>
 #include <wiringPi.h>
 
-#define GPIO14 14
 #define GPIO25 25
 
 int main(void) {
   if(wiringPiSetupGpio() == -1) {
     return -1;
   }
-  pinMode(GPIO14, OUTPUT);
   pinMode(GPIO25, INPUT);
   pullUpDnControl(GPIO25, PUD_UP);
 
   while(1) {
     if(digitalRead(GPIO25) == 0){
-      digitalWrite(GPIO14, 1);
-    } else {
-      digitalWrite(GPIO14, 0);
+      printf("Button Pressed\n");
     }
     delay(500);
   }
